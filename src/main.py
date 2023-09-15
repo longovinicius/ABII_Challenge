83a40475b612b97d300d7e8c4ea87396c1e99c32
longovinicius <longo.vinicius@gmail.com>
2023-09-15 05:26:30 -0300

    testing aruco read

--- src/main.py
+++ src/main.py
@@ -1,15 +1,32 @@
 from djitellopy import Tello
 from utils import cartesian_to_polar
+from marker import MarkerDetector, calculate_actual_distance_and_angle
 import time
+import cv2
 
 class ControleTello:
     def __init__(self, altura):
         self.tello = Tello()
         self.tello.connect()
+        self.tello.streamon()
         self.tello.takeoff()
         time.sleep(1)
         self.tello.move_up(altura)
         self.x, self.y, self.yaw = 0, 0, 0
+
+        # Initialize the MarkerDetector
+        frame_read = self.tello.get_frame_read()
+        self.marker_detector = MarkerDetector(frame_read.frame)
+
+    def process_frame_for_markers(self):
+        frame_read = self.tello.get_frame_read()
+        processed_frame, marker_data = self.marker_detector.process_frame(frame_read.frame)
+        if marker_data['Target'] is not None:
+            for info in marker_data['Target']:
+                print(f"Target ID: {info['id']}, Distance: {info['distance']}, X Offset: {info['x_offset']}")
+                print(calculate_actual_distance_and_angle(info['x_offset'], info['distance'], 50))
+        return processed_frame
+
     def missao_1(self):
         lista_coordenadas = []
         lista_coordenadas.append(((3, 0), 0))
@@ -38,6 +55,15 @@ class ControleTello:
             modulo, angulo = cartesian_to_polar(lista_coordenadas[i][0])
             print(angulo)
             print(modulo)
+
+            # Process the frame for markers
+            processed_frame = self.process_frame_for_markers()
+
+            # Display the processed frame (optional)
+            cv2.imshow("Processed Frame", processed_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
             if self.tello:
                 time.sleep(1)
                 self.tello.rotate_clockwise(angulo)
