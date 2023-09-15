bbd9594f702cf1fd1c0f1bb0646e8e74cd531610
valdomirojr <43709309+valdomirojr@users.noreply.github.com>
2023-09-15 08:17:03 -0300

    controle_tello_class

--- src/main.py
+++ src/main.py
@@ -2,24 +2,58 @@ from djitellopy import Tello
 from utils import cartesian_to_polar
 import time
 
-coordenada_objetivo = ((1, 3), 60)
+class ControleTello:
+    def __init__(self, altura):
+        self.tello = Tello()
+        self.tello.connect()
+        self.tello.takeoff()
+        time.sleep(1)
+        self.tello.move_up(altura)
+        self.x, self.y, self.yaw = 0, 0, 0
+    def missao_1(self):
+        lista_coordenadas = []
+        lista_coordenadas.append(((3, 0), 0))
+        lista_coordenadas.append(((0, -3), 90))
+        lista_coordenadas.append(((-2, 3), 180))
+        return lista_coordenadas
+    def missao_2(self):
+        lista_coordenadas = []
+        lista_coordenadas.append(((2, 0), 0))
+        lista_coordenadas.append(((0, 2), 0))
+        lista_coordenadas.append(((3, 0), 90))
+        lista_coordenadas.append(((1, 0), 0))
+        lista_coordenadas.append(((0, 1), 90))
+        lista_coordenadas.append(((0, 1), 0))
+        lista_coordenadas.append(((-1, 0), 90))
+        lista_coordenadas.append(((-2, 0), 90))
+        return lista_coordenadas
+    
+    def executar_missao(self, lista_coordenadas):
+        for i in range(len(lista_coordenadas)):
+            x_novo, y_novo = lista_coordenadas[i][0]
+            self.x += x_novo
+            self.y += y_novo
+            self.yaw = lista_coordenadas[i][1]
+            print(self.x, self.y, self.yaw)
+            modulo, angulo = cartesian_to_polar(lista_coordenadas[i][0])
+            print(angulo)
+            print(modulo)
+            if self.tello:
+                time.sleep(1)
+                self.tello.rotate_clockwise(angulo)
+                time.sleep(1)
+                self.tello.move_forward(modulo)
+                time.sleep(1)
+                self.tello.rotate_clockwise(lista_coordenadas[i][1])
 
+        if self.tello:
+            self.tello.land()
 
+if __name__ == "__main__":
+    altura_de_voo = 185
+    controle_tello = ControleTello(altura_de_voo)
+    
+    missao = controle_tello.missao_1()  # ou missao_1()
+    
+    controle_tello.executar_missao(missao)
 
-
-modulo, angulo = cartesian_to_polar(coordenada_objetivo[0])
-print(angulo)
-print(modulo)
-
-tello = Tello()
-tello.connect()
-tello.takeoff()
-time.sleep(1)
-tello.rotate_clockwise(angulo)
-time.sleep(1)
-tello.move_forward(modulo)
-time.sleep(1)
-tello.rotate_clockwise(coordenada_objetivo[1])
-
-
-tello.land()
