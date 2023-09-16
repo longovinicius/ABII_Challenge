a18a991360e0787c86b9ccbe8498bd5cfcecec8f
Luís Henrique Allebrandt Schünemann <44511825+LuisHenri@users.noreply.github.com>
2023-09-16 02:12:21 -0300

    refactor: refactor marker

--- src/marker.py
+++ src/marker.py
@@ -87,10 +87,10 @@ class MarkerDetector:
     def estimate_pose_and_draw_axes(
         self, frame: np.ndarray, corners: np.array, ids: list, marker_type: str
     ):
-        marker_info = []  # Define this at the beginning of your method
-
         if ids is None:
-            return
+            return None
+
+        marker_info = []  # Define this at the beginning of your method
 
         # Determine the marker width based on the type of marker
         marker_width = (
@@ -178,17 +178,17 @@ def calculate_actual_distance_and_angle(pixel_offset, z_distance, focal_length):
 
 #### WORKING MQTT
 
-import time
-start = time.time()
-subscriber = CronometerMQTT()
-subscriber.subscribe_topic("ProximoID")
-subscriber.subscribe_topic("TempoDecorrido")
-while True:
-    now = time.time()
-    TARGET_ID = subscriber.get_value_for_topic("ProximoID")
-    time_stamp = subscriber.get_value_for_topic("TempoDecorrido")
-    print(f"ID: {TARGET_ID}")
-    print(f"TIME: {time_stamp}")
-    time.sleep(1)
-    if now - start > 10:
-        subscriber.publish("StopCronometro","true")
+# import time
+# start = time.time()
+# subscriber = CronometerMQTT()
+# subscriber.subscribe_topic("ProximoID")
+# subscriber.subscribe_topic("TempoDecorrido")
+# while True:
+#     now = time.time()
+#     TARGET_ID = subscriber.get_value_for_topic("ProximoID")
+#     time_stamp = subscriber.get_value_for_topic("TempoDecorrido")
+#     print(f"ID: {TARGET_ID}")
+#     print(f"TIME: {time_stamp}")
+#     time.sleep(1)
+#     if now - start > 10:
+#         subscriber.publish("StopCronometro","true")
